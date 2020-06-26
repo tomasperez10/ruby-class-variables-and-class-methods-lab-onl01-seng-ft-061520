c15f85b1666e79d2b4e8dd65e662c476e2fafcdf
Tomas Perez <tomasitopg6@gmail.com>
2020-06-26 23:42:00 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,66 @@
+class Song
+  attr_accessor :name, :artist, :genre
+
+  
+  def initialize(song_name, artist, genre)
+    @name = song_name
+    @artist = artist
+    @@artists << artist
+    @genre = genre
+    @@genres << genre
+    @@count += 1
+  end
+  
+  @@count = 0
+  
+  def self.count
+    @@count
+  end
+  
+  @@genres = []
+  
+  def self.genres
+    @@genres.uniq!
+  end
+  
+  def self.genre_count
+    genre_count = {}
+    @@genres.map do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
+  
+  @@artists = []
+  
+  def self.artists
+    @@artists.uniq!
+  end
+  
+  def self.artist_count
+    artist_count = {}
+    @@artists.map do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
+  end
+  
+
+  
+  
+  
+end
+
+
+Song.genres
+Song.artists
+Song.genre_count
+Song.artist_count
